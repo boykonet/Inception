9f491017a88dc3fd1227c24320152d8ed10d2ea6
gkarina <gkarina@student.21-school.ru>
2021-12-05 20:44:47 +0300

    nginx make up\!

--- srcs/requirements/nginx/tools/nginx.sh
+++ srcs/requirements/nginx/tools/nginx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst < /files/nginx.template > /etc/nginx/conf.d/default.conf
+envsubst '${DOMAIN_NAME} ${CERTS_PATH} ${WORDPRESS_CONTAINER_NAME} ${WORDPRESS_PORT}' < /files/nginx.template > /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -7,7 +7,7 @@ if [ ! -f "$FILE" ]; then
 
   wp config create --dbname="$MYSQL_DATABASE" --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost="$MYSQL_CONTAINER_NAME:$MYSQL_PORT" --dbprefix="wp_" --path='/var/www/wordpress'
 
-  wp core install --url=https://$DOMAIN_NAME:$WORDPRESS_PORT --title='Wordpress site' --admin_user='gkarina' --admin_password='admin_password' --admin_email='gkarina@student.21-school.ru' --skip-email --path='/var/www/wordpress'
+  wp core install --url="https://$DOMAIN_NAME" --title='Wordpress site' --admin_user='gkarina' --admin_password='admin_password' --admin_email='gkarina@student.21-school.ru' --skip-email --path='/var/www/wordpress'
 
   wp user create boykonet boikosvet@yandex.ru --role='editor' --user_pass='user_password' --display_name='Svetlana Mikhaylenko' --nickname='boykonet' --first_name='Svetlana' --last_name='Mikhaylenko' --path='/var/www/wordpress'
 
